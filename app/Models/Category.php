3705dfc93988f0f0f18e34bd347545b2637c8951
alishahen22 <alishahen2016@gmail.com>
2025-07-29 17:05:19 +0300

    fix: Correct debug statement formatting in setImageAttribute method and add missing newline at the end of Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -62,6 +62,6 @@ class Category extends Model
             }
             $this->attributes['image'] = $imageFields;
         }
-        dd($image ,attributes['image']  );
+        dd($image ,  $this->attributes['image']  );
     }
 }
